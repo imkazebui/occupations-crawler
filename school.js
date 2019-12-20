7638e9cc4f6a589636e5f6d821c67c409f8d151c
PhuongBui <phuong.bui@saigontechnology.com>
2019-12-20 17:11:04 +0700

    update code

--- school.js
+++ school.js
@@ -11,14 +11,32 @@ const getListSchool = url => {
       let $ = cheerio.load(body);
 
       let listMajor = [];
-      let listSchool = [];
+
+      let schoolInAreaData = {};
 
       const length = $(".MsoNormalTable").length;
 
       $(".MsoNormalTable").each(function(idx, table) {
         if (idx + 1 !== length) {
+          let majorData = {
+            title: $(this)
+              .prev()
+              .text(),
+            list: []
+          };
+
+          $("tbody", table)
+            .children()
+            .each(function(trIndex, tr) {
+              let rowData = [];
+              $("td", tr).each(function(tdIndex, td) {
+                rowData.push($(this).text());
+              });
+              majorData.list.push(rowData);
+            });
+
+          listMajor.push(majorData);
         } else {
-          let data = {};
           let locationId = "";
 
           $("tbody", table)
@@ -40,7 +58,7 @@ const getListSchool = url => {
                       .children()
                       .first()
                       .text();
-                    data[locationId] = {
+                    schoolInAreaData[locationId] = {
                       locationName,
                       list: []
                     };
@@ -48,23 +66,54 @@ const getListSchool = url => {
                     if (schoolEl.text() !== "") {
                       schoolData.schoolName = schoolEl.text();
                       schoolData.link = schoolEl.attr("href");
+
+                      // get schoolContent
+                      request.get(schoolData.link, function(err, res, body) {
+                        if (err) {
+                          console.log(err);
+                        } else {
+                          let $ = cheerio.load(body);
+
+                          const schoolContent = $(".tabcontent")
+                            .first()
+                            .children()
+                            .filter(function(idx, el) {
+                              return (
+                                el.tagName === !"ins" &&
+                                !$(this).hasClass("itemblock") &&
+                                el.tagName === !"script"
+                              );
+                            })
+                            .map(function(idx, el) {
+                              console.log("daa", $(this).html());
+                              return $(this).html();
+                            })
+                            .get();
+                          // .join("<br />");
+                          console.log("schooll data content", schoolContent);
+                          schoolData.content = schoolContent;
+                          // return schoolContent;
+                        }
+                      });
                     } else {
                       schoolData.marjorId = marjorIdEl.text();
                     }
                   }
                 });
                 // console.log("school data", schoolData);
-                data[locationId].list.push(schoolData);
+                schoolInAreaData[locationId].list.push(schoolData);
               }
             });
-          fs.writeFile("school.txt", JSON.stringify(data), function(err) {
-            if (err) {
-              console.log(err);
-            } else {
-              console.log("success");
-            }
-          });
         }
+        fs.writeFile("school.txt", JSON.stringify(schoolInAreaData), function(
+          err
+        ) {
+          if (err) {
+            console.log(err);
+          } else {
+            console.log("success");
+          }
+        });
       });
     }
   });
@@ -101,8 +150,6 @@ module.exports = function() {
             link: $("a", el).attr("href")
           };
         });
-
-      // get school data
     }
   });
 };
